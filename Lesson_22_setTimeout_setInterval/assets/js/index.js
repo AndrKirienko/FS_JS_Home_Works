951d113fa772d1e140ad51c0dfc513bc212a1bad
ndrw <ndrw.iieo@gmail.com>
2024-08-23 23:43:05 +0300

    Add print link after time

--- Lesson_22_setTimeout_setInterval/assets/js/index.js
+++ Lesson_22_setTimeout_setInterval/assets/js/index.js
@@ -29,3 +29,28 @@ function printNumberTimeout(from, to, interval) {
 }
 
 printNumberTimeout(1, 5, 1000)
+
+//*Виводити посилання через певний час після завантаження сторінки. Поки посилання не відображається, на його місці виводити зворотній відлік "Зачекайте хвилин:секунд".
+
+function showLink(interval) {
+  const msgTime = document.querySelector('.msgTime')
+  const link = document.querySelector('a')
+  let remainingTime = interval
+
+  function updateTime() {
+    const minutes = Math.floor(remainingTime / 60)
+    const seconds = remainingTime % 60
+
+    msgTime.textContent = `Зачекайте ${minutes}:${seconds}`
+
+    if (remainingTime > 0) {
+      remainingTime--
+      setTimeout(updateTime, 1000)
+    } else {
+      msgTime.style.display = 'none'
+      link.style.display = 'inline'
+    }
+  }
+  updateTime()
+}
+showLink(100)

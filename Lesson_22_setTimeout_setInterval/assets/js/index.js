891ff9859c5fe23526a5b1016407c99d62385e54
ndrw <ndrw.iieo@gmail.com>
2024-08-23 23:16:48 +0300

    Add print number - setTimeout

--- Lesson_22_setTimeout_setInterval/assets/js/index.js
+++ Lesson_22_setTimeout_setInterval/assets/js/index.js
@@ -2,10 +2,9 @@
 
 //- Використовуючи setInterval
 function printNumber(from, to, interval) {
-  
-	const timeId = setInterval(printNumberToConsole, interval)
-  
-	function printNumberToConsole() {
+  const timeId = setInterval(printNumberToConsole, interval)
+
+  function printNumberToConsole() {
     console.log(from)
     if (from === to) {
       clearInterval(timeId)
@@ -16,5 +15,17 @@ function printNumber(from, to, interval) {
 
 printNumber(5, 10, 1000)
 
-
 //- *Використовуючи рекурсивний setTimeout
+function printNumberTimeout(from, to, interval) {
+  function printNumberTimeout() {
+    console.log(from, '-> setTimeout')
+
+    if (from < to) {
+      from++
+      setTimeout(printNumberTimeout, interval)
+    }
+  }
+  setTimeout(printNumberTimeout, interval)
+}
+
+printNumberTimeout(1, 5, 1000)

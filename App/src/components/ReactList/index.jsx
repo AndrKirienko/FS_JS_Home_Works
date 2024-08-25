f1c58dd756d9d6db40785fde18af64090d8a5cee
ndrw <ndrw.iieo@gmail.com>
2024-08-25 16:27:10 +0300

    Add styles for userList

--- App/src/components/ReactList/index.jsx
+++ App/src/components/ReactList/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import styles from './ReactList.module.sass'
 
-function ReactList({users}) {
-	// const { users } = props
-	// const { id, name } = users
-	console.log(users);
-	return <div>{users.map(u => <p>{u.name}</p>)}</div>
+function ReactList({ users }) {
+  return (
+    <section>
+      <ul className={styles.usersWrapper}>
+        {users.map(u => (
+          <li key={u.id} className={styles.userWrapperItem}>
+            <img src={u.image} alt={u.name} className={styles.userImg} />
+            <h2 className={styles.userFullName}>
+              <span>{u.name}</span>
+              <span>{u.surname}</span>
+            </h2>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
 }
 
 export default ReactList

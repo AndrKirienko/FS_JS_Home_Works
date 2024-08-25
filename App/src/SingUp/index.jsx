0e1495b927783f18b3b568438c6e95704b071ae8
ndrw <ndrw.iieo@gmail.com>
2024-08-25 19:07:28 +0300

    Add state for input

--- App/src/SingUp/index.jsx
+++ App/src/SingUp/index.jsx
@@ -5,23 +5,62 @@ function SingUp() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
+  const handleNameChange = e => {
+    setName(e.target.value)
+  }
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = e => {
+    setPassword(e.target.value)
+  }
+
   return (
     <section className={styles.singUpWrapper}>
       <form className={styles.formWrapper}>
         <h2 className={styles.nameForm}>Sing Up form</h2>
         <label className={styles.formLabel}>
           <span className={styles.formSpan}>Full name</span>
-          <input type="text" className={styles.formInput} />
+          <input
+            type="text"
+            name="name"
+            className={styles.formInput}
+            value={name}
+            onChange={handleNameChange}
+            placeholder="John Doe"
+            required
+            autoFocus
+          />
         </label>
         <label className={styles.formLabel}>
           <span className={styles.formSpan}>Email address</span>
-          <input type="email" className={styles.formInput} />
+          <input
+            type="email"
+            name="email"
+            className={styles.formInput}
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="johndor@gmail.com"
+            required
+          />
         </label>
         <label className={styles.formLabel}>
           <span className={styles.formSpan}>Password</span>
-          <input type="password" className={styles.formInput} />
+          <input
+            type="password"
+            name="password"
+            className={styles.formInput}
+            value={password}
+            onChange={handlePasswordChange}
+            placeholder="Password"
+            required
+          />
         </label>
-        <button type="submit" className={styles.singUpBtn}>Sing Up</button>
+        <button type="submit" className={styles.singUpBtn}>
+          Sing Up
+        </button>
       </form>
     </section>
   )

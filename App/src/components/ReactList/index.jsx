d206871ec7efbfd0e378f4270ffee932bff1b628
ndrw <ndrw.iieo@gmail.com>
2024-08-25 17:18:02 +0300

    Add functioin deleted user

--- App/src/components/ReactList/index.jsx
+++ App/src/components/ReactList/index.jsx
@@ -1,18 +1,29 @@
+import { useState } from 'react'
 import { CiTrash } from 'react-icons/ci'
 import styles from './ReactList.module.sass'
 
 function ReactList({ users }) {
+  const [userList, setUserList] = useState(users)
+
+  const handleRemoveUser = userId => {
+    const updatedUsers = userList.filter(user => user.id !== userId)
+    setUserList(updatedUsers)
+  }
+
   return (
     <section>
       <ul className={styles.usersWrapper}>
-        {users.map(u => (
+        {userList.map(u => (
           <li key={u.id} className={styles.userWrapperItem}>
             <img src={u.image} alt={u.name} className={styles.userImg} />
             <h2 className={styles.userFullName}>
               <span>{u.name}</span>
               <span>{u.surname}</span>
             </h2>
-						<button className={styles.trashBtn}>
+            <button
+              className={styles.trashBtn}
+              onClick={() => handleRemoveUser(u.id)}
+            >
               <CiTrash />
             </button>
           </li>

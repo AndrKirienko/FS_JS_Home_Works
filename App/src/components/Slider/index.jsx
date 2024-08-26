c1cf03cac95a6f46acef4ceb2260ea1754a7f392
ndrw <ndrw.iieo@gmail.com>
2024-08-26 18:31:28 +0300

    Add state for nextBtn and prevBtn

--- App/src/components/Slider/index.jsx
+++ App/src/components/Slider/index.jsx
@@ -1,18 +1,33 @@
+import { useState } from 'react'
 import { GrNext, GrPrevious } from 'react-icons/gr'
 import styles from './Slider.module.sass'
 import { slides } from './ImagesForSlider'
 
 function Slider() {
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
+
+  const nextSlide = () => {
+    setCurrentSlideIndex(index => (index === slides.length - 1 ? 0 : index + 1))
+  }
+
+  const prevSlide = () => {
+    setCurrentSlideIndex(index => (index === 0 ? slides.length - 1 : index - 1))
+  }
+
   return (
     <section className={styles.sliderWrapper}>
       <div className={styles.slider}>
-        <button className={styles.prevBtn}>
+        <button className={styles.prevBtn} onClick={prevSlide}>
           <GrPrevious />
         </button>
         <div className={styles.slide}>
-          <img className={styles.slideImg} src={slides[0].src} alt="" />
+          <img
+            className={styles.slideImg}
+            src={slides[currentSlideIndex].src}
+            alt=""
+          />
         </div>
-        <button className={styles.nextBtn}>
+        <button className={styles.nextBtn} onClick={nextSlide}>
           <GrNext />
         </button>
       </div>

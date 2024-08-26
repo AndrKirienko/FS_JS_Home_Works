972c6fcc185c63d6d358073adfaf5e6ca749cd3b
ndrw <ndrw.iieo@gmail.com>
2024-08-26 17:47:29 +0300

    Add images

---
+++ App/src/components/Slider/ImagesForSlider.jsx
@@ -0,0 +1,18 @@
+export const slides = [
+  {
+    id: 1,
+    src: 'https://tourbaza.com/wp-content/uploads/2015/10/%D0%A7%D0%BE%D1%80%D0%BD%D0%BE%D0%B3%D0%BE%D1%80%D0%B0-1-800x401.jpg',
+  },
+  {
+    id: 2,
+    src: 'https://vsviti.com.ua/wp-content/uploads/2016/02/2-3.jpg',
+  },
+  {
+    id: 3,
+    src: 'https://cdn.drukarnia.com.ua/6458c78cc149c83ef73680e5/images/articles/6458c8aac149c83ef7368404/bduO514463.26905879006.jpeg',
+  },
+  {
+    id: 4,
+    src: 'https://vsviti.com.ua/wp-content/uploads/2016/02/4-2.jpg',
+  },
+]
--- App/src/components/Slider/index.jsx
+++ App/src/components/Slider/index.jsx
@@ -1,6 +1,12 @@
-import styles from './Slider.module.sass'
+import './Slider.module.sass'
+import { slides } from './ImagesForSlider'
+
 function Slider() {
-  return <div>Slider</div>
+  return (
+    <div>
+      <img src={slides[3].src} alt="" />
+    </div>
+  )
 }
 
 export default Slider

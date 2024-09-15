255185ab31883ab3c07759b06c0322e21efcaee0
ndrw <ndrw.iieo@gmail.com>
2024-09-15 14:42:17 +0300

    Add updateTaskById

--- App/app.js
+++ App/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const { createTask } = require('./controllers/taskController')
+const {
+  createTask,
+  getTaskById,
+  updateTaskById,
+} = require('./controllers/taskController')
 const app = express().use(express.json())
 
 //app.get('/tasks', getTasks)
 app.post('/tasks', createTask)
-//app.get('/tasks/:id', getTaskById)
-//app.patch('/tasks/:id', updateTaskById)
+app.get('/tasks/:id', getTaskById)
+app.patch('/tasks/:id', updateTaskById)
 //app.delete('/tasks/:id', deleteTaskById)
 
 module.exports = app
--- App/controllers/taskController.js
+++ App/controllers/taskController.js
@@ -1,7 +1,35 @@
 const { tasks } = require('./../model')
 
 module.exports.createTask = (req, res) => {
-	const { body } = req
-	const createTask = tasks.createTask(body)
-	res.status(201).send(createTask)
+  const { body } = req
+  const createTask = tasks.createTask(body)
+  res.status(201).send(createTask)
+}
+
+module.exports.getTaskById = (req, res) => {
+  const {
+    params: { id },
+  } = req
+
+  const foundTask = tasks.getTaskById(id)
+  if (!foundTask) {
+    return res.status(404).send('Task Not Found')
+  }
+
+  res.status(200).send(foundTask)
+}
+
+module.exports.updateTaskById = (req, res) => {
+  const {
+    params: { id },
+    body,
+	} = req
+	
+	const updateTask = tasks.updateTask(id, body)
+
+	if (!updateTask) {
+		return res.status(404).send('Task Not Found')
+	}
+
+	res.status(200).send(updateTask)
 }

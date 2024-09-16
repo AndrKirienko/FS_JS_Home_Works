89ecbf81385102e35804d96d37567b93c4ff1b3d
ndrw <ndrw.iieo@gmail.com>
2024-09-16 17:02:04 +0300

    Test git sucsess

--- App/middleware/errorHandler.js
+++ App/middleware/errorHandler.js
@@ -14,5 +14,5 @@ module.exports.errorHandler = (err, req, res, next) => {
   const status = err.status ?? 500
   const message = err.message ?? 'Server Error'
 
-  res.status(status).send(mes
+  res.status(status).send(message)
 }

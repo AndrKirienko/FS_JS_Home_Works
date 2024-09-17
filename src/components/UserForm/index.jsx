344615115d1de504f33e62b93f5af3f3f3f48ad4
ndrw <ndrw.iieo@gmail.com>
2024-09-17 14:45:29 +0300

    Add validete schema

--- src/components/UserForm/index.jsx
+++ src/components/UserForm/index.jsx
@@ -1,23 +1,41 @@
 import { Formik } from 'formik'
 import React from 'react'
+import * as yup from 'yup'
+
+const USER_NAME_SCHEMA = yup.object({
+  userLogin: yup
+    .string()
+    .trim()
+    .min(2)
+    .max(32)
+    .matches(/^[A-Z]/)
+    .required(),
+})
 
 function UserForm() {
-  const initialValues = { userName: '' }
+  const initialValues = { userLogin: '' }
   const handleSubmit = (value, formikBag) => {
     formikBag.resetForm()
   }
 
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={USER_NAME_SCHEMA}
+    >
       {formikProps => {
         return (
           <form onSubmit={formikProps.handleSubmit}>
             <input
               type="text"
-              name="userName"
-              value={formikProps.values.userName}
+              name="userLogin"
+              value={formikProps.values.userLogin}
               onChange={formikProps.handleChange}
             />
+            {formikProps.errors.userLogin && (
+              <span>{formikProps.errors.userLogin}</span>
+            )}
             <button type="submit" disabled={!formikProps.dirty}>
               Ok
             </button>

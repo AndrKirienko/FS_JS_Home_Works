18e9227b20fbe9af21dce16736c25fa66d3872d3
ndrw <ndrw.iieo@gmail.com>
2024-09-17 12:37:59 +0300

    Add sinple route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-function App() {
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
+function App() {
   return (
-		<div>React</div>
-		
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/component">Component</Link>
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/component" element={<Component />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   )
 }
-
 export default App
+
+function Home() {
+  return <div>Home</div>
+}
+
+function Component() {
+  return <div>Component</div>
+}
+
+function About() {
+  return <div>About</div>
+}

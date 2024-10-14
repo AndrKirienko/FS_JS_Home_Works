3229508e05151e1da82078a09a6880943ceee256
ndrw <ndrw.iieo@gmail.com>
2024-10-14 12:22:36 +0300

    Add method next & prev slide

--- App/src/components/Slider/index.jsx
+++ App/src/components/Slider/index.jsx
@@ -4,16 +4,34 @@ import styles from './Slider.module.sass'
 import { slides } from './ImagesForSlider'
 
 class Slider extends Component {
+  constructor(props) {
+    super(props)
 
-	constructor(props) {
-		super(props)
-	
-		this.state = {
-			 currentSlideIndex: 0
-		}
-	}
-  // const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
+    this.state = {
+      currentSlideIndex: 0,
+    }
 
+    this.nextSlide = this.nextSlide.bind(this)
+    this.prevSlide = this.prevSlide.bind(this)
+  }
+
+  nextSlide() {
+    this.setState(prevState => ({
+      currentSlideIndex:
+        prevState.currentSlideIndex === slider.length - 1
+          ? 0
+          : prevState.currentSlideIndex + 1,
+    }))
+  }
+
+  prevSlide() {
+    this.setState(prevState => ({
+      currentSlideIndex:
+        prevState.currentSlideIndex === 0
+          ? slides.length - 1
+          : prevState.currentSlideIndex - 1,
+    }))
+  }
   // const nextSlide = () => {
   //   setCurrentSlideIndex(index => (index === slides.length - 1 ? 0 : index + 1))
   // }
@@ -22,24 +40,27 @@ class Slider extends Component {
   //   setCurrentSlideIndex(index => (index === 0 ? slides.length - 1 : index - 1))
   // }
 
-  render() {
-    return
-    ;<section className={styles.sliderWrapper}>
-      <div className={styles.slider}>
-        <button className={styles.prevBtn} onClick={prevSlide}>
-          <GrPrevious />
-        </button>
-        <img
-          key={slides.id}
-          className={styles.slideImg}
-          src={slides[currentSlideIndex].src}
-          alt=""
-        />
-        <button className={styles.nextBtn} onClick={nextSlide}>
-          <GrNext />
-        </button>
-      </div>
-    </section>
+	render() {
+		
+		
+    return (
+      <section className={styles.sliderWrapper}>
+        <div className={styles.slider}>
+          <button className={styles.prevBtn} onClick={prevSlide}>
+            <GrPrevious />
+          </button>
+          <img
+            key={slides.id}
+            className={styles.slideImg}
+            src={slides[this.state.currentSlideIndex].src}
+            alt=""
+          />
+          <button className={styles.nextBtn} onClick={nextSlide}>
+            <GrNext />
+          </button>
+        </div>
+      </section>
+    )
   }
 }
 

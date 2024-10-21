a5a25030f4fad2d996907787bfd0e856293c7843
ndrw <ndrw.iieo@gmail.com>
2024-10-21 12:20:05 +0300

    Add redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { legacy_createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
-import './index.css'
+
+const initialStateUser = {
+  nickname: 'Testovich',
+  isFavorite: false,
+}
+function reducer(state = initialStateUser, action) {
+  return state
+}
+
+const store = legacy_createStore(reducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )

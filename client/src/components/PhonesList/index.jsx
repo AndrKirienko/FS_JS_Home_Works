07c6687958e5a98898a4f2269f51742897847246
ndrw <ndrw.iieo@gmail.com>
2024-10-31 20:55:44 +0200

    Add paginatins for phones items

--- client/src/components/PhonesList/index.jsx
+++ client/src/components/PhonesList/index.jsx
@@ -1,28 +1,64 @@
 import { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
-import styles from './PhonesList.model.sass'
+import styles from './PhonesList.module.sass'
 
 function PhoneList() {
+  const initialResults = 5
+
   const [phones, setPhones] = useState([])
+  const [page, setPage] = useState(1)
+  const [results, setResults] = useState(initialResults)
+
+  const handlePrev = () => {
+    if (page > 1) {
+      setPage(page - 1)
+    }
+  }
+
+  const handleNext = () => {
+    setPage(page + 1)
+  }
+
+  const handleAddMore = () => {
+    setResults(results + initialResults)
+  }
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/phones?page=1&results=10')
+    fetch(`http://localhost:5000/api/phones?page=${page}&results=${results}`)
       .then(response => response.json())
       .then(({ data }) => {
         setPhones(data)
       })
       .catch(err => console.log(err))
-  }, [])
+  }, [page, results])
+
   return (
-    <>
-      <ul>
+    <div className={styles.wrapper}>
+      <ul className={styles.listWrapper}>
         {phones.map(p => (
-          <li key={p.id} className={styles.text}>
+          <li key={p.id} className={styles.phoneListItem}>
             {p.model}
           </li>
         ))}
       </ul>
-    </>
+      <div className={styles.addMore}>
+        <button
+          className={styles.btn}
+          onClick={handleAddMore}
+        >
+          Показати ще...
+        </button>
+      </div>
+      <div className={styles.pageList}>
+        <button className={styles.btn} onClick={handlePrev}>
+          prev
+        </button>
+        <span>{page}</span>
+        <button className={styles.btn} onClick={handleNext}>
+          next
+        </button>
+      </div>
+    </div>
   )
 }
 

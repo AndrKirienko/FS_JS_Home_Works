6f553b513ac856d1c6afd919c27486c08d067f64
ndrw <ndrw.iieo@gmail.com>
2024-11-04 16:56:53 +0200

    Add createPhoneThunk

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,3 +7,5 @@ export const getPhones = (page, results) =>
   httpClient.get(`/phones?page=${page}&results=${results}`)
 
 export const removePhone = payload => httpClient.delete(`/phones/${payload}`)
+
+export const createPhone = body => httpClient.post('/phones', body)
--- client/src/store/slices/phonesSlice.js
+++ client/src/store/slices/phonesSlice.js
@@ -44,22 +44,22 @@ export const removePhoneThunk = createAsyncThunk(
   }
 )
 
-// export const createUserThunk = createAsyncThunk(
-//   `${USERS_SLICE_NAME}/post`,
-//   async (payload, thunkApi) => {
-//     try {
-//       const {
-//         data: { data },
-//       } = await API.createUser(payload)
-//       return data
-//     } catch (error) {
-//       return thunkApi.rejectWithValue({
-//         status: error.response.status,
-//         message: error.response.data.errors,
-//       })
-//     }
-//   },
-// )
+export const createPhoneThunk = createAsyncThunk(
+  `${PHONES_SLICE_NAME}/post`,
+  async (payload, thunkApi) => {
+    try {
+      const {
+        data: { data },
+      } = await API.createPhone(payload)
+      return data
+    } catch (error) {
+      return thunkApi.rejectWithValue({
+        status: error.response.status,
+        message: error.response.date.errors,
+      })
+    }
+  }
+)
 
 const phonesSlice = createSlice({
   name: PHONES_SLICE_NAME,
@@ -80,71 +80,48 @@ const phonesSlice = createSlice({
     },
   },
   extraReducers: builder => {
-    //   //createUser
-    //   builder.addCase(createUserThunk.pending, (state, action) => {
-    //     state.isFetching = true
-    //     state.error = null
-    //   })
+    builder.addCase(createPhoneThunk.pending, state => {
+      state.error = null
+    })
 
-    //   builder.addCase(createUserThunk.rejected, (state, { payload }) => {
-    //     state.isFetching = false
-    //     state.users.push(payload)
-    //   })
+    builder.addCase(createPhoneThunk.rejected, (state, { payload }) => {
+      state.error = payload
+		})
+		
+    builder.addCase(createPhoneThunk.fulfilled, (state, { payload }) => {
+      state.phones.unshift(payload)
+    })
 
-    //   builder.addCase(createUserThunk.fulfilled, (state, { payload }) => {
-    //     state.isFetching = false
-    //     state.users = payload
-    //   })
-
-    //   //removeUser
-    //   builder.addCase(removeUserThunk.pending, (state, action) => {
-    //     state.isFetching = true
-    //     state.error = null
-    //   })
-
-    //   builder.addCase(removeUserThunk.rejected, (state, { payload }) => {
-    //     state.isFetching = false
-    //     const deletedUserIndex = state.users.findIndex(u => u.id === payload)
-    //     if (deletedUserIndex !== -1) {
-    //       state.users.splice(deletedUserIndex, 1)
-    //     }
-    //   })
-
-    //   builder.addCase(removeUserThunk.fulfilled, (state, { payload }) => {
-    //     state.isFetching = false
-    //     state.users = payload
-    //   })
-
-    builder.addCase(removePhoneThunk.pending, (state, action) => {
-      state.isFetching = true
+    builder.addCase(removePhoneThunk.pending, state => {
+      //state.isFetching = true
       state.error = null
     })
 
     builder.addCase(removePhoneThunk.rejected, (state, { payload }) => {
-      state.isFetching = false
+      // state.isFetching = false
       state.error = payload
     })
 
     builder.addCase(removePhoneThunk.fulfilled, (state, { payload }) => {
-      state.isFetching = false
+      // state.isFetching = false
       const removePhoneIndex = state.phones.findIndex(p => p.id === payload)
       if (removePhoneIndex !== -1) {
         state.phones.splice(removePhoneIndex, 1)
       }
     })
 
-    builder.addCase(getPhonesThunk.pending, (state, action) => {
-      state.isFetching = true
+    builder.addCase(getPhonesThunk.pending, state => {
+      //state.isFetching = true
       state.error = null
     })
 
     builder.addCase(getPhonesThunk.rejected, (state, { payload }) => {
-      state.isFetching = false
+      //state.isFetching = false
       state.error = payload
     })
 
     builder.addCase(getPhonesThunk.fulfilled, (state, { payload }) => {
-      state.isFetching = false
+      //state.isFetching = false
       state.phones = [...payload]
     })
   },

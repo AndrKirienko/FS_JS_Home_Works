b145f8efc8affba14915740c2e4fb923707265ad
ndrw <ndrw.iieo@gmail.com>
2024-11-06 15:06:34 +0200

    Add query create 5 movies

---
+++ cinemaQueries.mongodb.js
@@ -0,0 +1,131 @@
+use('cinema')
+
+//TODO Додайте нові фільми до колекції movies, щоб загальна кількість фільмів була не менше 7.
+// db.movies.insertMany([
+//   {
+//     title: 'The Intouchables',
+//     genre: 'Comedy Drama',
+//     director: 'Olivier Nakache & Éric Toledano',
+//     releaseYear: 2011,
+//     duration: 112,
+//     ratings: [
+//       { reviewer: 'Sophia Lee', score: 9 },
+//       { reviewer: 'Jake Anderson', score: 8 },
+//     ],
+//     cast: ['François Cluzet', 'Omar Sy', 'Anne Le Ny'],
+//     boxOffice: {
+//       budget: 10500000,
+//       revenue: 426600000,
+//     },
+//     country: 'France',
+//     tags: ['friendship', 'inspiring', 'uplifting'],
+//   },
+
+//   {
+//     title: 'Crouching Tiger',
+//     genre: 'Martial Arts',
+//     director: 'Ang Lee',
+//     releaseYear: 2000,
+//     duration: 120,
+//     ratings: [
+//       { reviewer: 'Ella Collins', score: 10 },
+//       { reviewer: 'Victor Green', score: 9 },
+//     ],
+//     cast: ['Chow Yun-Fat', 'Michelle Yeoh', 'Zhang Ziyi'],
+//     boxOffice: {
+//       budget: 17000000,
+//       revenue: 213500000,
+//     },
+//     country: 'China',
+//     tags: ['epic', 'fantasy', 'romance'],
+//   },
+
+//   {
+//     title: 'City of God',
+//     genre: 'Crime Drama',
+//     director: 'Fernando Meirelles & Kátia Lund',
+//     releaseYear: 2002,
+//     duration: 130,
+//     ratings: [
+//       { reviewer: 'Carlos Silva', score: 9 },
+//       { reviewer: 'Ana Rodrigues', score: 10 },
+//     ],
+//     cast: ['Alexandre Rodrigues', 'Leandro Firmino', 'Phellipe Haagensen'],
+//     boxOffice: {
+//       budget: 3300000,
+//       revenue: 30600000,
+//     },
+//     country: 'Brazil',
+//     tags: ['gangs', 'violence', 'realism'],
+//   },
+
+//   {
+//     title: 'Pan’s Labyrinth',
+//     genre: 'Fantasy',
+//     director: 'Guillermo del Toro',
+//     releaseYear: 2006,
+//     duration: 118,
+//     ratings: [
+//       { reviewer: 'Natalie Foster', score: 10 },
+//       { reviewer: 'Oscar Morales', score: 9 },
+//     ],
+//     cast: ['Ivana Baquero', 'Ariadna Gil', 'Sergi López'],
+//     boxOffice: {
+//       budget: 19000000,
+//       revenue: 83500000,
+//     },
+//     country: 'Spain',
+//     tags: ['dark fantasy', 'fairy tale', 'war'],
+//   },
+
+//   {
+//     title: 'Oldboy',
+//     genre: 'Thriller',
+//     director: 'Park Chan-wook',
+//     releaseYear: 2003,
+//     duration: 120,
+//     ratings: [
+//       { reviewer: 'Tom Wright', score: 9 },
+//       { reviewer: 'Hanna Kim', score: 10 },
+//     ],
+//     cast: ['Choi Min-sik', 'Yoo Ji-tae', 'Kang Hye-jung'],
+//     boxOffice: {
+//       budget: 3000000,
+//       revenue: 15000000,
+//     },
+//     country: 'South Korea',
+//     tags: ['revenge', 'psychological', 'mystery'],
+//   },
+// ])
+
+//TODO Відсортувати фільми за тривалістю у зростаючому порядку та показати тільки назву та тривалість.
+
+//TODO Отримати другу сторінку при перегляді по 3 фільми на сторінці, впорядкувавши за роком випуску від найновіших до найстаріших.
+
+//TODO Знайдіть усі фільми в жанрі "Sci-Fi", * випущені після 2000 року.
+
+//TODO Знайти всі фільми, випущені після 2000 року, і показати тільки їх назву, режисера та рік випуску.
+
+//TODO *Знайти фільми режисера "Christopher Nolan", тривалість яких перевищує 150 хвилин.
+
+//TODO *Показати другу сторінку результатів для фільмів у жанрі "Action", з 2 фільмами на сторінку.
+
+//TODO *Відобразити фільми з бюджетом, більше 10 млн.
+
+//TODO *Відобразити фільми з тегами 'thriller' та 'superhero'
+
+//TODO Оновити тривалість фільму "Inception" до 150 хвилин.
+
+//TODO *Додати новий тег "blockbuster" до фільму "The Dark Knight". (оператор $push)
+
+//TODO Видалити фільм "The Godfather" з колекції.
+
+//TODO * Видалити всі фільми, випущені до 1980 року.
+
+//TODO *Підрахуйте середню тривалість фільмів у кожному жанрі.
+
+//TODO * Знайдіть жанр із найбільшою кількістю фільмів.
+
+//TODO * Підрахуйте загальні збори(revenue) фільмів за країнами.
+
+//TODO *Підрахувати кількість фільмів, знятих в кожній країні після 2010 року.

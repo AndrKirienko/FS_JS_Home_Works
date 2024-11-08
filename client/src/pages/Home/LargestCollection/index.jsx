795d2013b0756757f19d271681841fad6137c141
ndrw <ndrw.iieo@gmail.com>
2024-11-08 14:30:41 +0200

    Add subtitle & styles for LargestCollection

--- client/src/pages/Home/LargestCollection/index.jsx
+++ client/src/pages/Home/LargestCollection/index.jsx
@@ -1,9 +1,17 @@
-import styles from './LargestCollection.module.sass'
+import styles from './LargestCollection.module.sass';
 
 function LargestCollection() {
-	return (<div className={styles.collectionContainer}>
-		<h3 className={styles.collectionTitle}>Largest Collection of Brandable Names</h3>
-	</div>);
+  return (
+    <div className={styles.collectionContainer}>
+      <h3 className={styles.collectionTitle}>
+        Largest Collection of Brandable Names
+      </h3>
+      <p className={styles.collectionSubTitle}>
+        Explore themed brand name collections created by our experienced team of
+        branding experts
+      </p>
+    </div>
+  );
 }
 
 export default LargestCollection;

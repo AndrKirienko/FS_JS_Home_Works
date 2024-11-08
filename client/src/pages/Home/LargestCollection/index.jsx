558ea28065b07d6e0016db589b38c7ae7ebd70dd
ndrw <ndrw.iieo@gmail.com>
2024-11-08 15:39:14 +0200

    Add initial tabs

--- client/src/pages/Home/LargestCollection/index.jsx
+++ client/src/pages/Home/LargestCollection/index.jsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
+import classNames from 'classnames';
 import styles from './LargestCollection.module.sass';
 
 function LargestCollection() {
+  const [toggleState, setToggleState] = useState('categories');
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  const contentTab = (value) => {
+    return classNames({
+      [styles.contentTab]: true,
+      [styles.contentTabActive]: toggleState === value,
+    });
+  };
+
+  const buttonTabs = (value) => {
+    return classNames({
+      [styles.buttonsTab]: true,
+      [styles.buttonTabActive]: toggleState === value,
+    });
+  };
   return (
     <div className={styles.collectionContainer}>
       <h3 className={styles.collectionTitle}>
@@ -11,17 +32,30 @@ function LargestCollection() {
         branding experts
       </p>
       <div className={styles.buttonsTabContainer}>
-        <button className={`${styles.buttonTab} ${styles.buttonTabActive}`}>
+        <button
+          className={buttonTabs('categories')}
+          onClick={() => toggleTab('categories')}
+        >
           Top Categories
         </button>
-        <button className={styles.buttonsTab}>Top Industries</button>
-        <button className={styles.buttonsTab}>Top Ideas</button>
-			</div>
-			<div className={styles.contentTabContainer}>
-				<div className={styles.contentTab}>1</div>
-				<div className={styles.contentTab}>2</div>
-				<div className={styles.contentTab}>3</div>
-			</div>
+        <button
+          className={buttonTabs('industries')}
+          onClick={() => toggleTab('industries')}
+        >
+          Top Industries
+        </button>
+        <button
+          className={buttonTabs('ideas')}
+          onClick={() => toggleTab('ideas')}
+        >
+          Top Ideas
+        </button>
+      </div>
+      <div className={styles.contentTabContainer}>
+        <div className={contentTab('categories')}>1</div>
+        <div className={contentTab('industries')}>2</div>
+        <div className={contentTab('ideas')}>3</div>
+      </div>
     </div>
   );
 }

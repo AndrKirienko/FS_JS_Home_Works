30134ba4d3dd5257635f0fd42d9ed94c42391a46
ndrw <ndrw.iieo@gmail.com>
2024-11-08 11:57:34 +0200

    Reformat project with prettier-standard

--- server/src/seeders/123123123-Bank.js
+++ server/src/seeders/123123123-Bank.js
@@ -8,24 +8,24 @@ module.exports = {
           name: 'SquadHelp',
           expiry: '11/26',
           cvc: '453',
-          balance: 0
+          balance: 0,
         },
         {
           cardNumber: '4111111111111111',
           name: 'yriy',
           expiry: '09/26',
           cvc: '505',
-          balance: 1000000
+          balance: 1000000,
         },
         {
           cardNumber: '5105105105105100',
           name: 'creative',
           expiry: '09/26',
           cvc: '510',
-          balance: 0
-        }
+          balance: 0,
+        },
       ],
       {}
-    )
-  }
-}
+    );
+  },
+};
